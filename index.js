fea8c94c827caac29419ac745feb954f52d7eba1
Mohammad Ashif <Mohammad Ashif>
2024-03-23 01:17:05 +0600

    store setup and increment done

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
-input {createStore} from 'redux';
+import { createStore } from "redux";
 
-// store
-const store = createStore(reducer); 
+// store 
+const store = createStore(reducer);
 
-
-// reducer
-function reducer(state, action){
+function reducer(state={amount: 1}, action){
+    if(action.type === "increment"){
+        return state.amount = {amount:state.amount + 1};
+    }
     return state;
 }
+
+console.log(store.getState());
+store.dispatch({type: "increment"})
+console.log(store.getState());
+
 
