f194cb83759e035f77a8dbeceb971b42a892640e
Mohammad Ashif <Mohammad Ashif>
2024-03-23 02:34:56 +0600

    increment, decrement, incrementByAmout function added

--- index.js
+++ index.js
@@ -1,17 +1,48 @@
-import { createStore } from "redux";
+import { createStore, applyMiddleware } from "redux";
+import logger from "redux-logger";
 
-// store 
-const store = createStore(reducer);
+// action name 
+const INC = 'increment'
+const DEC = 'decrement'
+const INC_BY_AMOUNT = 'incrementByPayload'
 
-function reducer(state={amount: 1}, action){
-    if(action.type === "increment"){
-        return state.amount = {amount:state.amount + 1};
+// store
+const store = createStore(reducer, applyMiddleware(logger.default));
+
+let history = [];
+
+
+function reducer(state = { amount: 1 }, action) {
+    if (action.type === INC) {
+        return {amount:state.amount + 1};
+    }
+
+    if (action.type === DEC) {
+        return {amount:state.amount - 1};
+    }
+    if (action.type === INC_BY_AMOUNT) {
+        return {amount:state.amount + action.payload};
     }
     return state;
 }
 
-console.log(store.getState());
-store.dispatch({type: "increment"})
-console.log(store.getState());
+// store.subscribe(() => {
+//     history.push(store.getState())
+//     console.log(history);
+// });
 
+// action creator
 
+function increment(){
+    return { type: INC }
+}
+function decrement(){
+    return { type: DEC }
+}
+function incrementByPayload(value){
+    return { type: INC_BY_AMOUNT, payload: value }
+}
+
+setInterval(() => {
+    store.dispatch(decrement());
+}, 1000);
